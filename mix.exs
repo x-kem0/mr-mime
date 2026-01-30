b2290aabfb52252a47bf5bbe5632f747995a4ddc
x <kemo@sent.com>
2026-01-29 21:50:28 -0600

    pre-publish

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule MrMime.MixProject do
   def project do
     [
       app: :mr_mime,
+      description: description(),
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,6 +25,17 @@ defmodule MrMime.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "Provides functions to identify a given file's MIME-type using file signatures and extensions."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/x-kem0/mr-mime/"}
     ]
   end
 end
